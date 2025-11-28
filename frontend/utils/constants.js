650e7d929cb39f0eae4e0500cb88c9232f2f7104
p4rth <parthsingh1006@gmail.com>
2025-11-28 17:54:28 +0530

    added constants

---
+++ frontend/utils/constants.js
@@ -0,0 +1,17 @@
+export const CATEGORIES = [
+  "All",
+  "Computer Science",
+  "Mathematics",
+  "Physics",
+  "Chemistry",
+  "Biology",
+  "Engineering",
+  "Literature",
+  "History",
+  "Business",
+  "Other",
+];
+
+export const LISTING_TYPES = ["All", "Sale", "Exchange", "Donation"];
+
+export const STATUS_OPTIONS = ["Available", "Sold"];
